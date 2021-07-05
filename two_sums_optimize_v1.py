dd4aabd737593645b1eae0bb5b0c752a6a3446c3
quan97it <33288275+quan97it@users.noreply.github.com>
2021-07-05 23:53:43 +0700

    Update two_sums_optimize_v1.py

--- two_sums_optimize_v1.py
+++ two_sums_optimize_v1.py
@@ -1,9 +1,10 @@
-class Solution:
+ # create a dict with keys are values of List and values are indexes of List
+class Solution: 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        res={}
+        res={}        #create Dict        
         for i in nums:
-           res[i] = nums.index(i)
+           res[i] = nums.index(i)   
         for j in range(len(nums)):
            sub = target - nums[j]
-           if sub in res and j != res[sub]:
+           if sub in res and j != res[sub]: #check if "sub" in "res" => return res[sub] (value of Dict) is one of idexes of List
               return [j,res[sub]]
